6ed4347f3222110d64298b16225078fb3adc61a8
Robson Lunardelli <robson@l9s.dev>
2020-09-07 01:19:14 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
 # laravel-wsl2
+
+# laravel-wsl
+
+Bootstrap script to configure a Laravel **development** environment on Windows Systems for Linux. For WSL setup, checkout the [laravel-wsl](https://github.com/rlunardelli/laravel-wsl) repository.
+
+## Setup
+
+### Enable WSL2
+
+Follow the instructions in this [documentation](https://docs.microsoft.com/en-us/windows/wsl/install-win10). Once you get WSL enabled, go ahead and install Debian.
+
+### Bootstrap script
+
+Once your WSL is up and running, download the bootstrap script and run it on your WSL. It will perform the following tasks:
+
+1. Update Linux
+2. Install and configure PHP 7.3
+3. Install MariaDB
+4. Install Redis
+5. Install composer and valet
+6. Install and configure vim
+7. Configure xdebug
+
+**Note**: The boostrap script was tested only on a Debian WSL.
+
+### Docroot
+
+Create a docroot where your code will exist. Since the Windows filesystem can be accessed from the WSL `/mnt/`, create a directory shared between the two spaces and symlinked it from your home directory in WSL.
+
+```
+ln -s /mnt/c/Users/<username>/<code_directory> ~/code
+```
+
+Run `valet` from within this new directory:
+
+```
+valet park
+```
+
+### Init Laravel
+
+In WSL, navigate to the your code directory and the run the following:
+
+```
+composer create-project --prefer-dist laravel/laravel blog
+```
+
+Composer will create a `blog` folder will all the boilerplate. You should be able to access it at: ``http://blog.test``.
+
+### VS Code
+
+To get all the VS Code goodness to work with PHP on your Windows box, you need to download the extension [Remote WSL](https://code.visualstudio.com/remote-tutorials/wsl/run-in-wsl). VS Code will add itself to your `%PATH%`, so from the WSL console you can type `code .` to open VS Code on that folder. If for some reason, VS Code is not in your path, you can modify your `~/.bashrc` and add VS Code to your path. By default, VS Code is installed under `C:\users\{username}\AppData\Local\Programs\Microsoft VS Code`.
+
+Add this line to your path:
+
+```
+export PATH=$PATH:/mnt/c/Users/{username}/AppData/Local/Programs/Microsoft VS Code/bin"
+```
+
+#### Terminal
+
+In case you need to configure VS Code Shell to use the WSL environment, you'll have to update the user's profile settings.
+
+* `File -> Preferences -> Settings`
+
+Then, you'll change/add the following property:
+
+```
+"terminal.integrated.shell.windows": "C:\\Windows\\Sysnative\\bash.exe"
+```
+
+## License
+
+This project is licensed under the Apache-2.0 License - see the [LICENSE](LICENSE) file for details
---
+++ src/install.sh
@@ -0,0 +1,300 @@
+#! /bin/bash
+
+#====================================================================================
+#
+#	FILE: bootstrap.sh
+#
+#	DESCRIPTION: Bootstrap Laravel Dev Environment for WSL2 Debian
+#
+#	LICENSE: Apache 2.0
+#
+#====================================================================================
+set -o errexit			# exit script when command fails
+
+# PHP Version to install
+PHP="7.4"
+NODEJS="lts"
+
+#---  FUNCTION  ----------------------------------------------------------------------
+#          NAME:  __check_command_exists
+#   DESCRIPTION:  Check if a command exists.
+#-------------------------------------------------------------------------------------
+__check_command_exists() {
+    command -v "$1" > /dev/null 2>&1
+}
+
+#---  FUNCTION  ----------------------------------------------------------------------
+#          NAME:  __check_debian
+#   DESCRIPTION:  Check if is Debian.
+#-------------------------------------------------------------------------------------
+__check_debian() {
+    cat /etc/os-release | grep -v grep | grep Debian > /dev/null 2>&1
+}
+
+#---  FUNCTION  ----------------------------------------------------------------------
+#          NAME:  __check_service_running
+#   DESCRIPTION:  Check if a given service is running.
+#-------------------------------------------------------------------------------------
+__check_service_running() {
+    ps -ef | grep -v grep | grep "$1" > /dev/null 2>&1
+}
+
+#---  FUNCTION  ----------------------------------------------------------------------
+#          NAME:  __set_colors  
+#   DESCRIPTION:  Set terminal colors
+#-------------------------------------------------------------------------------------
+__set_colors() {
+    RC="\e[1;31m"
+    GC="\e[1;32m"
+    YC="\e[1;33m"
+    BC="\e[1;34m"
+    WC="\e[0m"
+}
+__set_colors
+
+#---  FUNCTION  ----------------------------------------------------------------------
+#          NAME:  echoinfo  
+#   DESCRIPTION:  format output info
+#-------------------------------------------------------------------------------------
+function echoinfo() {
+    echo -e "${WC}[ ${GC}INFO${WC}  ] $@"
+}
+
+#---  FUNCTION  ----------------------------------------------------------------------
+#          NAME:  echoerror  
+#   DESCRIPTION:  format output error
+#-------------------------------------------------------------------------------------
+function echoerror() {
+    echo -e "${WC}[ ${RC}ERROR${WC} ] $@"
+}
+
+#---  FUNCTION  ----------------------------------------------------------------------
+#          NAME:  echowarn  
+#   DESCRIPTION:  format output warn
+#-------------------------------------------------------------------------------------
+function echowarn() {
+    echo -e "${WC}[ ${YC}WARN${WC}  ] $@"
+}
+
+#-------------------------------------------------------------------------------------
+#  Simple warning notification and validation
+#-------------------------------------------------------------------------------------
+if [ __check_debian ]; then
+    echowarn "This script has only been tested on a Debian system. Run it at your own discretion."
+fi
+
+echowarn "This setup is NOT suited for production deployments.\n"
+sleep 5
+
+#-------------------------------------------------------------------------------------
+# Update local OS
+#-------------------------------------------------------------------------------------
+echoinfo "Update OS"
+sudo apt-get update -y &> /dev/null
+sudo apt-get upgrade -y &> /dev/null
+
+#-------------------------------------------------------------------------------------
+# Install OS packages
+#-------------------------------------------------------------------------------------
+echoinfo "Install PHP and supporting libraries"
+sudo apt-get install -y php-xdebug \
+                        php-fpm \
+                        php-mysql \
+                        php$PHP-cli \
+                        php$PHP-sqlite \
+                        php$PHP-curl \
+                        php$PHP-mbstring \
+                        php$PHP-xml \
+                        php$PHP-zip \
+                        php$PHP-intl \
+                        php$PHP-bcmath \
+                        php$PHP-ctype \
+                        php$PHP-json \
+                        curl git unzip \
+                        php-cli \
+                        network-manager \
+                        libnss3-tools \
+                        jq xsel vim &> /dev/null
+
+#-------------------------------------------------------------------------------------
+# NodeJs + NPM
+#-------------------------------------------------------------------------------------
+
+# https://github.com/nodesource/distributions/blob/master/README.md
+echoinfo "Install Nodejs"
+curl -sL https://deb.nodesource.com/setup_lts.x | sudo -E bash - &> /dev/null
+sudo apt-get install -y nodejs &> /dev/null
+
+#-------------------------------------------------------------------------------------
+# Composer
+#-------------------------------------------------------------------------------------
+echoinfo "Download and install composer"
+cd ~
+curl -sS https://getcomposer.org/installer -o composer-setup.php &> /dev/null
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer &> /dev/null
+
+echoinfo "Configure and update composer"
+if [[ ! -e ~/.composer ]]; then
+    mkdir ~/.composer
+fi
+sudo echo "{}" > ~/.composer/composer.json
+sudo chown -R $USER ~/.composer
+
+# check if composer command exists
+if ! __check_command_exists composer; then
+    echoerror "Command 'composer' doesn't exist. Make sure it was correctly installed."
+    exit 1
+fi
+
+# check if composer config exists
+if [ ! -f ~/.composer/composer.json ]; then
+    echoerror "Composer configuration file doesn't exist."
+    exit 1
+fi
+
+echoinfo "Update composer"
+composer global update &> /dev/null
+
+echoinfo "Install composer prestissimo"
+composer global require hirak/prestissimo &> /dev/null
+
+echoinfo "Setup Laravel Installer"
+composer global require laravel/installer &> /dev/null
+
+cat >> ~/.bashrc << EOF
+export PATH="$PATH:$HOME/.composer/vendor/bin"
+EOF
+
+#-------------------------------------------------------------------------------------
+# Valet
+#-------------------------------------------------------------------------------------
+
+#echoinfo "Install valet"
+composer global require cpriego/valet-linux
+~/.composer/vendor/bin/valet install 
+
+# check valet command exists
+
+if [ ! -f "~/.composer/vendor/bin/valet" ]; then
+    echoerror "Valet command doesn't exist. Review your composer install."
+    exit 1
+fi
+
+#-------------------------------------------------------------------------------------
+# Database
+#-------------------------------------------------------------------------------------
+echoinfo "Install Mariadb Server"
+sudo apt-get install -qq mariadb-server &> /dev/null
+
+echoinfo "Configure Mariadb Server"
+sudo debconf-set-selections <<< "maria-db mysql-server/root_password password "
+sudo debconf-set-selections <<< "maria-db mysql-server/root_password_again password "
+
+sudo sed -i "s/bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mariadb.conf.d/50-server.cnf
+sudo service mysql restart &> /dev/null
+
+MYSQL=`which mysql`
+
+Q1="use mysql;"
+Q2="update user set plugin='' where User='root';"
+Q3="update user set host='%' where User='root';"
+Q4="flush privileges;"
+SQL="${Q1}${Q2}${Q3}${Q4}"
+
+sudo $MYSQL -uroot -e "$SQL"
+sudo service mysql restart &> /dev/null
+
+if ! __check_service_running mysql; then
+    echowarn "MYSQL couldn't be started. Review MySQL logs."
+fi
+
+#-------------------------------------------------------------------------------------
+# Redis
+#-------------------------------------------------------------------------------------
+echoinfo "Install Redis Server"
+sudo apt-get install -y redis-server &> /dev/null
+sudo service redis-server start &> /dev/null
+
+if ! __check_service_running redis-server; then
+    echowarn "Redis server couldn't be started. Review Redis logs."
+fi
+
+#-------------------------------------------------------------------------------------
+# CTL Services
+#-------------------------------------------------------------------------------------
+echoinfo "Create startup script"
+SCRIPT_FILE="ctl-services.sh"
+
+cat > ~/${SCRIPT_FILE} << EOF
+#!/bin/bash
+case "${1:-''}" in
+  'start');;
+  'stop');;
+  'restart');;
+  'status');;
+  *)
+    echo "Usage: $SELF start|stop|restart|status"
+    exit 1
+  ;;
+esac
+SERVICES=( php-fpm nginx mysql redis-server )
+for i in "${SERVICES[@]}"
+do
+  if [ $i == 'php-fpm' ]
+    then
+        sudo service php7.3-fpm $1
+    else
+        sudo service $i $1
+  fi
+done
+EOF
+
+chmod +x ~/"${SCRIPT_FILE}"
+
+if [ ! -f "${SCRIPT_FILE}" ]; then
+    echowarn "${SCRIPT_NAME} was not created."
+fi
+
+#-------------------------------------------------------------------------------------
+# VIM
+#-------------------------------------------------------------------------------------
+echoinfo "Update vim configuration"
+mkdir -p ~/.vim/colors
+curl -o ~/.vim/colors/gruvbox.vim -O https://raw.githubusercontent.com/morhetz/gruvbox/master/colors/gruvbox.vim &> /dev/null
+cat >> ~/.vimrc << EOF
+set number
+syntax enable
+set background=dark
+colorscheme gruvbox
+set mouse=a
+if &term =~ '256color'
+" disable Background Color Erase (BCE) so that color schemes
+" render properly when inside 256-color tmux and GNU screen.
+" see also http://snk.tuxfamily.org/log/vim-256color-bce.html
+set t_ut=
+endif
+EOF
+
+echo "alias vi=vim" >> ~/.bashrc
+source ~/.bashrc
+
+#-------------------------------------------------------------------------------------
+# XDEBUG
+#-------------------------------------------------------------------------------------
+echoinfo "Configure xdebug"
+cat << EOF | sudo tee -a /etc/php/$PHP/fpm/conf.d/20-xdebug.ini &> /dev/null
+xdebug.default_enable=1
+xdebug.remote_enable=1
+xdebug.remote_connect_back=1
+xdebug.remote_port=9001
+xdebug.scream=0
+xdebug.cli_color=1
+xdebug.show_local_vars=1
+xdebug.remote_autostart=1
+EOF
+
+sudo service php$PHP-fpm restart &> /dev/null
+
+if ! __check_service_running fpm ; then
+    echowarn "php-fpm service couldn't be started."
+fi
